bada01fb4abaf49a6f23e5cef69491e4c48d8e0f
5d9_100006509127_liaoliao <5d9_100006509127_liaoliao@git.cloud.tencent.com>
2018-09-11 18:45:15 +0800

    1.2(发布功能)

--- server/controllers/pub_question.js
+++ server/controllers/pub_question.js
@@ -25,9 +25,6 @@ module.exports = async (ctx, next) => {
       var result3 = await DB('PQ_community').insert({ community_id: power.detail_power[i].community_id, object_type: 1, object_id: question_id, power: power.detail_power[i].power, time: date });
     }
   }
-
-
-  
   //例如
   //var x = await DB('question').insert({ user_type: 0, user_id: 18211949726, question_title: '公司如何解决员工问题', question_info: '我们公司是一个小公司.....', question_time: date, answernum: 0, power: 1, hot: 100 });
   
