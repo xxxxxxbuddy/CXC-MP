712082c136f43a5e81597693f2c04c6e33dac01c
5d9_100006509127_liaoliao <5d9_100006509127_liaoliao@git.cloud.tencent.com>
2018-09-24 20:22:25 +0800

    再更新

--- server/controllers/pubed_answer.js
+++ server/controllers/pubed_answer.js
@@ -19,12 +19,14 @@ module.exports = async (ctx, next) => {
     for(var i=0;i<answer.length;i++){
       if(answer[i].object_type==0){
         var project = await DB.select('*').from('project').where('project_id',answer[i].object_id);
+        result[i]={};
         result[i].object_type=0;
         result[i].object_info=project;
         result[i].info=answer[i];
       }
       else{
         var question = await DB.select('*').from('question').where('question_id', answer[i].object_id);
+        result[i] = {};
         result[i].object_type = 1;
         result[i].object_info = question;
         result[i].info = answer[i];
