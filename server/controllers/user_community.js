8628ed40ae100150c9ac6e6e7b5c2f44cdefa202
zlforever21 <691997599@qq.com>
2018-09-29 18:04:22 +0800

    community of main

--- server/controllers/user_community.js
+++ server/controllers/user_community.js
@@ -16,12 +16,13 @@ module.exports = async (ctx, next) => {
   var data = ctx.query;
   var community_id = '';
   var result=[];
-  community_id = await DB.select('community_id').from('community_user')//.where({ user_id: data.user_id, user_type: data.user_type });
-  for(var i=0;i<community_id.length;i++){
-    var x = await DB.select('*').from('community').where('community_id', community_id[i].community_id);
-    result[i]=x[0];
-    result[i].checklabel=0;
-  }
+  result = await DB.select('*').from('community')
+  community_id = await DB.select('community_id').from('community_user').where({ user_id: data.user_id, user_type: data.user_type });
+   for(var i=0;i<community_id.length;i++){
+     var x = await DB.select('*').from('community').where('community_id', community_id[i].community_id);
+     result[i]=x[0];
+     result[i].checklabel=0;
+   }
   ctx.body = {
     result: result
   }
