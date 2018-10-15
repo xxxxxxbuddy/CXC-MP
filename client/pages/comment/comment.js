5e042bd5f7c47edf160b7bed03c859f7801e4798
xxxxxxbuddy <691997599@qq.com>
2018-10-15 20:52:31 +0800

    to merge

--- client/pages/comment/comment.js
+++ client/pages/comment/comment.js
@@ -1,4 +1,5 @@
 var config=require("./../../config.js")
+const app = getApp();
 function timeCalc(time) {
   var now = new Date()
   var timeDiff = parseInt((now - time) / 1000)     //单位为秒
@@ -34,7 +35,9 @@ Page({
    */
   data: {
     commentList: '',
-    praise_url: 'http://tmp/wx2541631ee62bb5d9.o6zAJs5rFdB4S1Go-WOFbowYbp8k.5AyHAKDRmITC86f35fd4ad1fe0d8e675204e5aa1ba81.png'
+    praise_url: app.globalData.praise_url,
+    back_url: app.globalData.back_url,
+    id: {}
   },
 
   /**
@@ -53,7 +56,7 @@ Page({
         var i=0;
         for(var item of res.data.result){
           item.comment_time = new Date(item.comment_time.replace(/T/, " ").replace(/Z/, "").replace(/-/g, "/"))
-          item.comment_time.comment_time = timeCalc(item.comment_time);
+          item.comment_time = timeCalc(item.comment_time);
           i=i+1;
         } 
         that.setData({
@@ -88,7 +91,29 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+    var object_id = [];
+    var that = this;
 
+    if (that.data.id) {
+      for (var item in that.data.id) {
+        if (that.data.id[item]) {
+          object_id.push(item)
+        }
+      }
+      console.log(object_id)
+      wx.request({
+        url: config.service.praise,
+        data: {
+          user_type: app.globalData.userInfo.user_type,
+          user_id: app.globalData.userInfo.user_id,
+          object_type: 1,
+          object_id: object_id
+        },
+        success: function (res) {
+          console.log(res.data)
+        }
+      })
+    }
   },
 
   /**
@@ -110,5 +135,46 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  jumpToHome: function (e) {
+    console.log(e)
+    wx.navigateTo({
+      url: './../home/home?userType=' + e.target.dataset.type + '&userId=' + e.currentTarget.id,
+    })
+  },
+  like: function (e) {
+    console.log(e)
+    var that = this
+
+    let id = e.target.dataset.id
+    if (that.data.id[id]) {
+      let count = -1
+      for (var com of that.data.commentList) {
+        count++
+        if (com.comment_id == id) {
+          that.data.commentList[count].praisenum--;
+          that.data.id[id] = false;
+          that.setData({
+            commentList: that.data.commentList,
+          })
+          break
+        }
+      }
+
+    }
+    else {
+      let count = -1
+      for (var com of that.data.commentList) {
+        count++
+        if (com.comment_id == id) {
+          that.data.commentList[count].praisenum++;
+          that.data.id[id] = true;
+          that.setData({
+            commentList: that.data.commentList,
+          })
+          break
+        }
+      }
+    }
   }
 })
